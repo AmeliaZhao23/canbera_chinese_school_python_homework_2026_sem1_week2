a49f7256f5fb94f2b381de650a95fd0592460650
Amelia <yinggezhao23@gmail.com>
2026-02-14 00:58:43 +1100

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+# =========================
+# Week 2 Homework
+# =========================
+
+# -------------------------
+# Part 0 – Write Your Name
+# -------------------------
+
+# TODO: Write your name inside the quotes
+student_name = ""
+
+print("Student Name:", student_name)
+
+# -------------------------
+# Part 1 – Multiple Choice
+# -------------------------
+
+# Write your answers below.
+# Example: answer1 = "A"
+
+# TODO: Write your answer for Question 1
+answer1 = ""
+
+# TODO: Write your answer for Question 2
+answer2 = ""
+
+# TODO: Write your answer for Question 3
+answer3 = ""
+
+print("Your answers:")
+print("Q1:", answer1)
+print("Q2:", answer2)
+print("Q3:", answer3)
+
+# -------------------------
+# Part 2 – Coding Task
+# Random Story Generator
+# -------------------------
+
+import random
+
+# TODO: Create a list of characters (at least 3)
+characters = []
+
+# TODO: Create a list of places (at least 3)
+places = []
+
+# TODO: Create a list of actions (at least 3)
+actions = []
+
+# TODO: Use random.choice() to pick one character
+character = None
+
+# TODO: Use random.choice() to pick one place
+place = None
+
+# TODO: Use random.choice() to pick one action
+action = None
+
+# TODO: Print the full random story sentence
